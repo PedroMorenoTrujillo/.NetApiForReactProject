8ae8ff4a497dc2850880ed38fe7079175631803a
PedroMorenoTrujillo <43439808+PedroMorenoTrujillo@users.noreply.github.com>
2020-10-06 17:06:11 +0200

    Added migrations and use classes in Dominio to create tables in db

--- Persistencia/CursosOnlineContext.cs
+++ Persistencia/CursosOnlineContext.cs
@@ -1,16 +1,18 @@
 using Dominio;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace Persistencia
 {
-    public class CursosOnlineContext : DbContext
+    public class CursosOnlineContext : IdentityDbContext<Usuario>
     {
         public CursosOnlineContext(DbContextOptions options) : base(options){
 
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<CursoInstructor>().HasKey(ci => new { ci.InstructorId, ci.CursoId });
         }
 
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistencia;
 
 namespace WebAPI
 {
@@ -13,7 +16,18 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostserver = CreateHostBuilder(args).Build();
+            using(var ambiente = hostserver.Services.CreateScope()){
+                var services = ambiente.ServiceProvider;
+                try{
+                    var context = services.GetRequiredService<CursosOnlineContext>();
+                    context.Database.Migrate();
+                }catch(Exception ex){
+                    var logging = services.GetRequiredService<ILogger<Program>>();
+                    logging.LogError(ex, "Ocurrio un error en la migracion");
+                }
+            }
+            hostserver.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
