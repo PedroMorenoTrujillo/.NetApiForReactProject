d657a0e2f5365452713a51cff3d1fa60c6f4a73d
PedroMorenoTrujillo <43439808+PedroMorenoTrujillo@users.noreply.github.com>
2020-10-07 17:37:52 +0200

    Create Json web Token

--- Aplicacion/Seguridad/Login.cs
+++ Aplicacion/Seguridad/Login.cs
@@ -1,54 +1,52 @@
-using System.Resources;
 using System.Net;
+using System.Resources;
 using System.Threading;
 using System.Threading.Tasks;
+using Aplicacion.Contratos;
 using Aplicacion.ManejadorError;
 using Dominio;
 using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 
-namespace Aplicacion.Seguridad
-{
-    public class Login
-    {
-        public class Ejecuta : IRequest<UsuarioData>{
+namespace Aplicacion.Seguridad {
+    public class Login {
+        public class Ejecuta : IRequest<UsuarioData> {
             public string Email { get; set; }
             public string Password { get; set; }
         }
 
-        public class Manejador : IRequestHandler<Ejecuta, UsuarioData>
-        {
+        public class Manejador : IRequestHandler<Ejecuta, UsuarioData> {
             private readonly UserManager<Usuario> _userManager;
             private readonly SignInManager<Usuario> _signInManager;
-            public Manejador(UserManager<Usuario> userManager, SignInManager<Usuario> signInManager){
+            private readonly IJwtGenerador _jwtGenerador;
+            public Manejador (UserManager<Usuario> userManager, SignInManager<Usuario> signInManager, IJwtGenerador jwtGenerador) {
                 _userManager = userManager;
                 _signInManager = signInManager;
+                _jwtGenerador = jwtGenerador;
             }
-            public class EjecutaValidacion : AbstractValidator<Ejecuta>{
-                public EjecutaValidacion(){
-                    RuleFor(x => x.Email).NotEmpty();
-                    RuleFor(x => x.Password).NotEmpty();
+            public class EjecutaValidacion : AbstractValidator<Ejecuta> {
+                public EjecutaValidacion () {
+                    RuleFor (x => x.Email).NotEmpty ();
+                    RuleFor (x => x.Password).NotEmpty ();
                 }
             }
-            public async Task<UsuarioData> Handle(Ejecuta request, CancellationToken cancellationToken)
-            {
-                var usuario = await _userManager.FindByEmailAsync(request.Email);
-                if (usuario == null){
-                    throw new ManejadorExcepcion(HttpStatusCode.Unauthorized);
+            public async Task<UsuarioData> Handle (Ejecuta request, CancellationToken cancellationToken) {
+                var usuario = await _userManager.FindByEmailAsync (request.Email);
+                if (usuario == null) {
+                    throw new ManejadorExcepcion (HttpStatusCode.Unauthorized);
                 }
-                var resultado = await _signInManager.CheckPasswordSignInAsync(usuario, request.Password, false);
-                if(resultado.Succeeded){
-                    return new UsuarioData
-                    {
+                var resultado = await _signInManager.CheckPasswordSignInAsync (usuario, request.Password, false);
+                if (resultado.Succeeded) {
+                    return new UsuarioData {
                         NombreCompleto = usuario.NombreCompleto,
-                        Token = "Esta sera la data del token",
+                        Token = _jwtGenerador.CrearToken(usuario),
                         UserName = usuario.UserName,
                         Email = usuario.Email,
                         Imagen = null
                     };
                 }
-                throw new ManejadorExcepcion(HttpStatusCode.Unauthorized);
+                throw new ManejadorExcepcion (HttpStatusCode.Unauthorized);
             }
         }
     }
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Aplicacion.Contratos;
 using Aplicacion.Cursos;
 using Dominio;
 using FluentValidation.AspNetCore;
@@ -19,53 +20,50 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistencia;
+using Seguridad;
 using WebAPI.Middleware;
 
-namespace WebAPI
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace WebAPI {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
             //Agregar el contexto para la base de datos y añadir la cadena de conexion que usara sqlServer
             //La cadena de conexion viene del archivo appsettings.json
-            services.AddDbContext<CursosOnlineContext>(opt =>
-            {
-                opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<CursosOnlineContext> (opt => {
+                opt.UseSqlServer (Configuration.GetConnectionString ("DefaultConnection"));
             });
 
             //Agergar el MediatR
-            services.AddMediatR(typeof(Consulta.Manejador).Assembly);
+            services.AddMediatR (typeof (Consulta.Manejador).Assembly);
 
             //services.AddControllers()
             //Configuramos el nuevo metodo con la libreria fluent para las validaciones
-            services.AddControllers().AddFluentValidation(cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo>());
+            services.AddControllers ().AddFluentValidation (cfg => cfg.RegisterValidatorsFromAssemblyContaining<Nuevo> ());
 
             //Configuracion necesaria para que funcione el CoreIdentity desde la API
-            var builder = services.AddIdentityCore<Usuario>();
-            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
-            identityBuilder.AddEntityFrameworkStores<CursosOnlineContext>();
-            identityBuilder.AddSignInManager<SignInManager<Usuario>>();
-            services.TryAddSingleton<ISystemClock, SystemClock>();
+            var builder = services.AddIdentityCore<Usuario> ();
+            var identityBuilder = new IdentityBuilder (builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<CursosOnlineContext> ();
+            identityBuilder.AddSignInManager<SignInManager<Usuario>> ();
+            services.TryAddSingleton<ISystemClock, SystemClock> ();
+
+            //Una vez creada la carpeta seguridad ...
+            services.AddScoped<IJwtGenerador, JwtGenerador> ();
 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
             //middleware para la gestion de errores personalizados
-            app.UseMiddleware<ManejadorErrorMiddleware>();
+            app.UseMiddleware<ManejadorErrorMiddleware> ();
 
-            if (env.IsDevelopment())
-            {
+            if (env.IsDevelopment ()) {
                 //Como hay creado un middleware no voy a usar este metodo
                 //app.UseDeveloperExceptionPage();
             }
@@ -73,13 +71,12 @@ namespace WebAPI
             //Comentado porque solo se usa en ambiente de produccion
             //app.UseHttpsRedirection();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
